4c7457ebdf6e1ffed5d23f5eef8c71c037baba03
Prasanjit Singh <mailprasanjit@gmail.com>
2021-05-07 23:25:04 +0400

    Update ItemClass.java

--- monolith/app/src/main/java/works/weave/socks/broker/items/ItemClass.java
+++ monolith/app/src/main/java/works/weave/socks/broker/items/ItemClass.java
@@ -3,6 +3,6 @@ package works.weave.socks.broker.items;
 public class ItemClass {
 
   public static String getItems() {
-    return "Coffee,Uber,Movie,Popcorn:200,150,200,100";
+    return "Coffee,Uber,Movie:200,150,200";
   }
 }
